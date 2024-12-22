0faa9611f8da10016e6943e4e92f470ce8e428b7
ceelos01 <theonlycarlos123@gmail.com>
2024-12-21 18:46:45 -0600

    Add files via upload

---
+++ login.js
@@ -0,0 +1,23 @@
+// login.js
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.getElementById('login-form');
+    loginForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+
+        if (username === 'sandra' && password === 'sigma') {
+            playSound('wow-sound-effect.mp3');
+            alert('WOOOOW YOU GOT IT RIGHT! Ojala te gusta!');
+            window.location.href = 'index.html'; // Redirect to the main content page
+        } else {
+            alert('Incorrect input. Think');
+        }
+    });
+});
+
+function playSound(soundFile) {
+    const audio = new Audio(soundFile);
+    audio.play();
+}
